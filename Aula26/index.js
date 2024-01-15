05c726254c153b72b404d56fdd22e972507c412d
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-01-15 17:50:11 -0300

    math

---
+++ Aula26/index.js
@@ -0,0 +1,11 @@
+let num1 = 9.4375;
+
+let num2 = math.floor(num1);
+let num3 = math.ceil(num1);
+let num4 = math.round(num1);//arredonda o número
+
+
+console.log(Math.max(num1, num2, num3, num4));//maior numero
+
+
+const aleatorio = math.round(math.random() * (10 - 5) + 5); 
