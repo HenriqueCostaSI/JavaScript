3fcfd2fd2c4deb01531d4e74b539b9bc114036c5
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-01-15 14:43:14 -0300

    strings

---
+++ Aula24/script.js
@@ -0,0 +1,10 @@
+//strings sao indexadas
+
+let umaString = "Um texto";
+
+console.log(umaString.chartAt(8));
+console.log(umaString.indexOf("texto"));
+console.log(umaString.slice(2));
+console.log(umaString.split(" "));
+console.log(umaString.toLocaleUpperCase());
+
