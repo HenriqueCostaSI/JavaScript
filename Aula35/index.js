0145301aa146aa738dadc757ed4f1e8ec00c8ca6
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-04-12 19:44:58 -0300

    Operadores

---
+++ Aula35/index.js
@@ -0,0 +1,25 @@
+/*
+Operadores de comparação
+> maior
+>= maior ou igual
+<menor
+<=menor igual
+==igualdade (valor)
+=== igualdade estrito (valor e tipo)
+!= diferente (valor)
+!== diferente estrito(valor e tipo) 
+*/
+console.log(5 > 3); // true
+const comp  = 10 > 5 ; // true
+console.log(comp);
+const num1 = 10;
+const num2 = "10";
+
+const comp2 = num1 === num2; // false
+
+/*
+Operadores lógicos
+&& (and) e -> Todas Verdadeiras
+ || (or) ou
+! (not) negação
+*/
