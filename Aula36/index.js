1df10924f5ffad451f64147deb75519ec6c0cd3b
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-04-12 20:50:01 -0300

    If e else

---
+++ Aula36/index.js
@@ -0,0 +1,12 @@
+const num = 10;
+
+if (num>= 0 && num <= 5){
+    console.log('O número está entre 0 e 5');
+
+} else if (num > 5 && num <= 10) {
+    console.log('O número  está entre 5 e 10');
+} else {
+    console.log('O número não está entre 0 e 10');
+}
+
+// A partir do encontro de uma expressao verdadeira o if nao e mais executado
