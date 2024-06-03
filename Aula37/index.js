558e2d38491ef610a9358b2a0ccaac13de852c2b
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-06-03 17:38:52 -0300

    Operador Ternario

---
+++ Aula37/index.js
@@ -0,0 +1,23 @@
+/**
+ ? : - > (condicao) ? (valor true) : (valor false)
+**/
+
+const pontuacaoUsuario = 999;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? "VIP" : "Normal";
+
+console.log(nivelUsuario);
+
+
+//if (pontuacaoUsusario >+ 1000) {
+//    console.log("Voce é um usuario vip");
+//}else {
+//    console.log("Voce é um usuario normal");
+
+//}
+
+
+const corUsuario = null;
+const corPadrao = corUsuario || "Preto";
+
+//utilizando um valor padrao
+
