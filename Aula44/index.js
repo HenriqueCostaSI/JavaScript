f86c1b834fd01b9ddecc6d7963b2fbb8a03a869b
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-06-06 15:25:05 -0300

    for

---
+++ Aula44/index.js
@@ -0,0 +1,12 @@
+//for
+
+for (let i = 0; i < 10; i++) {
+    const num = i % 2 === 0 ? 'par' : 'ímpar';
+    console.log(`${i} é par? ${num}`);
+}
+
+const frutas = ['Banana', 'Maçã', 'Laranja', 'Uva'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
