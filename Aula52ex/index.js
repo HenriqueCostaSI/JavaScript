288726bd9bcebde81140df9061b74331eac4290c
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-06-08 01:11:45 -0300

    *

---
+++ Aula52ex/index.js
@@ -0,0 +1,9 @@
+//Escreva uma função que recebe 2 números e retorne o maior deles
+
+function maiorNumero(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
+console.log(maiorNumero(10, 20));
+
+
+const menorNumero = (x,y) => x < y? x : y;
