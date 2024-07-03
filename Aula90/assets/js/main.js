2d45059db0a3c391313944fbf973f727c8917734
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-07-02 21:44:53 -0300

    Adicionando comentarios

--- Aula90/assets/js/main.js
+++ Aula90/assets/js/main.js
@@ -1,3 +1,5 @@
+/* The class ValidaFormulario is used to validate form fields, including checking if passwords match,
+validating usernames, and verifying CPF numbers. */
 class ValidaFormulario {
   constructor() {
     this.formulario = document.querySelector(".formulario");
@@ -21,6 +23,13 @@ class ValidaFormulario {
     }
   }
 
+/**
+ * The function `senhasSaoValidas` checks if the password and repeat password fields are equal and if
+ * the password length is between 6 and 12 characters.
+ * @returns The function `senhasSaoValidas()` is returning a boolean value indicating whether the
+ * passwords are valid or not. If the passwords do not match or if the length of the password is not
+ * between 6 and 12 characters, the function will return `false`. Otherwise, it will return `true`.
+ */
   senhasSaoValidas() {
     let valid = true;
 
@@ -44,6 +53,13 @@ class ValidaFormulario {
     return valid;
   }
 
+  /**
+   * The function "camposSaoValidos" checks the validity of form fields, displaying error messages for
+   * empty fields and validating CPF and username inputs.
+   * @returns The `camposSaoValidos()` function returns a boolean value indicating whether all the
+   * fields in the form are valid. If all fields are valid, it returns `true`, otherwise it returns
+   * `false`.
+   */
   camposSaoValidos() {
     let valid = true;
 
@@ -71,6 +87,15 @@ class ValidaFormulario {
     return valid;
   }
 
+/**
+ * The function "validaUsuario" checks if a user input meets certain criteria for length and character
+ * composition.
+ * @param campo - Campo is a parameter representing the input field where the username is entered. The
+ * function `validaUsuario(campo)` takes this input field as an argument and performs validation checks
+ * on the username entered in that field. It checks the length of the username and ensures it contains
+ * only letters and numbers. If any
+ * @returns The `valid` variable is being returned from the `validaUsuario` function.
+ */
   validaUsuario(campo) {
     const usuario = campo.value;
     let valid = true;
@@ -91,6 +116,16 @@ class ValidaFormulario {
     return valid;
   }
 
+/**
+ * The function `validaCPF` validates a CPF (Brazilian individual taxpayer registry identification)
+ * number inputted in a form field.
+ * @param campo - The `campo` parameter in the `validaCPF` function is likely referring to a field or
+ * input element that contains a CPF (Cadastro de Pessoas Físicas) number, which is a Brazilian
+ * individual taxpayer identification number. The function `validaCPF` is responsible for validating
+ * this CPF
+ * @returns The `validaCPF` function is returning a boolean value - `true` if the CPF is valid, and
+ * `false` if the CPF is invalid.
+ */
   validaCPF(campo) {
     const cpf = new ValidaCPF(campo.value);
 
