476c6f46827ecbc37d6c7365042d5e05b4d6071d
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-08-29 01:13:49 -0300

    Express - req.params

--- Node/Aula08/server.js
+++ Node/Aula08/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 
-// CRUD -> Create, Read, Update, Delete
-//          Post   Get   Put     Delete
+app.use(
+  express.urlencoded(
+    { extended: true }
+  )
+);
 
-// http://meuSite.com/ -> get  entregue a pagina inicial
-// http://meuSite.com/contato -> get  entregue a pagina de contato
-
-//Uma rota para tratar as requisições do tipo GET no '/'
+//http://facebook.com/profiles/123
 app.get("/", function (req, res) {
   // req = request, res = response
   res.send(`
@@ -18,15 +18,18 @@ app.get("/", function (req, res) {
     `);
 });
 
-app.post("/", (req, res) => {
-  res.send("Recebi o formulário");
+
+app.get('/teste:@idUsuarios?/:parametroOpcionais', (req, res) => {
+  console.log(req.params);//retorna um objeto com os parametros
+  console.log(req.query);//retorna um objeto com os query
+  res.send(req.params.idUsuarios);
 });
 
-app.get("/contato", (res, req) => {
-  res.send("Obrigado por entrar em contato");
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send(`O que você me enviou foi: ${req.body.nome}`);
 });
-
-//uma porta serve para receber requisições
+  
 app.listen(3000, function () {
   console.log("Acessar http://localhost:3000");
 });
