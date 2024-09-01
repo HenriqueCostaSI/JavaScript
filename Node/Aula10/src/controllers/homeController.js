21b33d6a5d5db37c53583e47a8fce82e11163362
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-08-31 23:11:09 -0300

    Express

--- Node/Aula10/src/controllers/homeController.js
+++ Node/Aula10/src/controllers/homeController.js
@@ -1,11 +1,5 @@
 exports.paginaInicial = (req, res) => {
-    res.send(`
-        <form action="/" method="POST">
-        Nome: <input type = "text" name="nome"
-        Outro campo: <input type = "text" name="outrocampo">
-        <button>Enviar</button>
-        </form>
-        `);
+    res.render('index');
 };
 
 exports.trataPost = (req, res) => {
