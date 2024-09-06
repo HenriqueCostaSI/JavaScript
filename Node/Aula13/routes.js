45734e2bdf9cb999d41d117044dcf014a60c57d4
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-09-06 20:46:01 -0300

    exemplo de middlewares

--- Node/Aula13/routes.js
+++ Node/Aula13/routes.js
@@ -3,8 +3,20 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const contatoController = require('./src/controllers/contatoController');
 
+//middleware-> função que é executada antes de qualquer rota.
+function meuMiddleware(req, res, next) {
+    req.session = {nome: 'Guilherme', sobrenome: 'Moraes'};
+    console.log();
+    console.log('Passei no seu middleware');
+    console.log(``);
+    next();//se não chamar o next, a requisição não será finalizada.
+}
+
 // Rotas da home
-route.get('/', homeController.paginaInicial);
+route.get('/',meuMiddleware ,homeController.paginaInicial, (req, res, next) => {
+    console.log('Estou aqui na home');
+    console.log();
+});
 route.post('/', homeController.trataPost);
 
 // Rotas de contato
--- Node/Aula13/src/controllers/homeController.js
+++ Node/Aula13/src/controllers/homeController.js
@@ -1,7 +1,10 @@
-exports.paginaInicial = (req, res) => {
+exports.paginaInicial = (req, res, next) => {
+    console.log('entrei na pagina inicial');
     res.render('index');
+    console.log('Olha oo que tem dentro de req.session');
+    next();//vai para o proximo middleware
 };
 
-exports.trataPost = (req, res) => {
+exports.trataPost = (req, res, next) => {
     res.send('Ei, sou sua nova rota de POST.');
 };
