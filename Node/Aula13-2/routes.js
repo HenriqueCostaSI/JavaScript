78b02b72ee4c1a745a3eaf9707de6f16827995eb
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-09-10 16:06:33 -0300

    Aula13

--- Node/Aula13-2/routes.js
+++ Node/Aula13-2/routes.js
@@ -3,20 +3,8 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const contatoController = require('./src/controllers/contatoController');
 
-//middleware-> função que é executada antes de qualquer rota.
-function meuMiddleware(req, res, next) {
-    req.session = {nome: 'Guilherme', sobrenome: 'Moraes'};
-    console.log();
-    console.log('Passei no seu middleware');
-    console.log(``);
-    next();//se não chamar o next, a requisição não será finalizada.
-}
-
 // Rotas da home
-route.get('/',meuMiddleware ,homeController.paginaInicial, (req, res, next) => {
-    console.log('Estou aqui na home');
-    console.log();
-});
+route.get('/', homeController.paginaInicial);
 route.post('/', homeController.trataPost);
 
 // Rotas de contato
--- Node/Aula13-2/src/controllers/homeController.js
+++ Node/Aula13-2/src/controllers/homeController.js
@@ -1,10 +1,16 @@
-exports.paginaInicial = (req, res, next) => {
-    console.log('entrei na pagina inicial');
-    res.render('index');
-    console.log('Olha oo que tem dentro de req.session');
-    next();//vai para o proximo middleware
-};
-
-exports.trataPost = (req, res, next) => {
-    res.send('Ei, sou sua nova rota de POST.');
-};
+exports.middlewareGlobal = (req, res, next) => {
+    if (req.body.cliente) {
+  
+      console.log();
+  
+      console.log(`Vi que você postou ${req.body.cliente}`);
+  
+      console.log();
+  
+    }
+    next();
+  };
+  
+  exports.outroMiddleware = (req, res, next) => {
+    next();
+  };
