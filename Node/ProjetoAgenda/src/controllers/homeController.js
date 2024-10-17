7c800a520cc62f533c73f68f146e925dd31623cb
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-10-16 23:17:03 -0300

    Criando pagina de login

--- Node/ProjetoAgenda/src/controllers/homeController.js
+++ Node/ProjetoAgenda/src/controllers/homeController.js
@@ -1,12 +1,4 @@
-exports.paginaInicial = (req, res) => {
-  // console.log(req.session.nome);
-  req.flash('info', 'Olá mundo');//mesagem que dura uma autalizacao de pg
-  console.log(req.flash('info'));
+exports.index = (req, res) => {
   res.render('index');
-  return;
-};
-
-exports.trataPost = (req, res) => {
-  res.send(req.body);
-  return;
+  
 };
