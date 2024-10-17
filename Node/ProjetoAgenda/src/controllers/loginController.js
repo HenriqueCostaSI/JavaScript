c95acef6c0e2e3a8687d26186361e14c3040920c
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-10-17 20:05:05 -0300

    $

--- Node/ProjetoAgenda/src/controllers/loginController.js
+++ Node/ProjetoAgenda/src/controllers/loginController.js
@@ -1,31 +1,28 @@
-const Login = require('../models/LoginModel');
- 
+const Login = require("../models/LoginModel");
+
 exports.index = (req, res) => {
-    res.render('login');
+  res.render("login");
 };
- 
-exports.register = async function(req, res) { 
-    try {
+
+exports.register = async function (req, res) {
+  try {
     const login = new Login(req.body);
     await login.register();
- 
-    if(login.errors.length > 0) {
-        req.flash('errors', login.errors);
-        req.session.save(function () {//Salva a sessão e redireciona para a rota
-           return res.redirect('/login/index');
-        });
+
+    if (login.errors.length > 0) {
+      req.flash("errors", login.errors);
+      req.session.save(function () {
+        return res.redirect("/login/index");
+      });
       return;
     }
-    
-    req.flash('success', 'Seu usuário foi criado com sucesso.');
-    req.session.save(function () {
-      return res.redirect('/login/index');
-    });
-    
 
-  
-  }  catch(e) {
+    req.flash("success", "Seu usuário foi criado com sucesso.");
+    req.session.save(function () {
+      return res.redirect("/login/index");
+    });
+  } catch (e) {
     console.log(e);
-    return res.render('404');
-    }
+    return res.render("404");
+  }
 };
