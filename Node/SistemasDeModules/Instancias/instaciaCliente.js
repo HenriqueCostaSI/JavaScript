f724c566bf6a72d386e3d91de1c1786a756f2327
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-11-28 16:08:05 -0300

    Tesets

--- Node/SistemasDeModules/Instancias/instaciaCliente.js
+++ Node/SistemasDeModules/Instancias/instaciaCliente.js
@@ -1,9 +1,15 @@
-const contadorA = require('./instanciaIUnica');
-const contadorB = require('./instanciaIUnica');
+const contadorA = require("./instanciaIUnica");
+const contadorB = require("./instanciaIUnica");
 
-const contadoC = require('./instanciaNova')()
+const contadoC = require("./instanciaNova")();
+const contadorD = require("./instanciaNova")();
 
-contadorA.inc()
-contadorA.inc()
+contadorA.inc();
+contadorA.inc();
 
 console.log(contadorB.valor);
+
+contadoC.inc();
+contadoC.inc();
+
+console.log(contadorD.valor);
