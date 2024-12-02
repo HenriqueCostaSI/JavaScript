018088acd1e9049abd7860d6b98a727e9d79778f
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-12-02 12:41:25 -0300

    Teste

--- Node/SistemasDeModules/Projeto/src/server.js
+++ Node/SistemasDeModules/Projeto/src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3003;
 
 app.get('/produtos', (req, res, next) => {
     res.send({ nome: 'Caneta', preco: 1.99 });// Converter para JSON
