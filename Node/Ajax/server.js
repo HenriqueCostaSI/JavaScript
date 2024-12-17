0b0a1dd4724c2a13b0b51a5ef4b8f00ab4a92a22
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-12-16 23:29:24 -0300

    xml2

--- Node/Ajax/server.js
+++ Node/Ajax/server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');//
 const express = require('express');
 const app = express();
+const multer = require('multer');
 
 
 //Middleware
@@ -10,6 +11,27 @@ app.use(bodyParser.json());
 
 app.get('/teste', (req, res) => {
     res.send('Ok');
+});
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './uploads');
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+
+const upload = multer({ storage }).single('arquivo');
+
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro.');
+        }
+        res.end('Concluído com sucesso.');
+    });
 });
 
 app.listen(3000, () => {
